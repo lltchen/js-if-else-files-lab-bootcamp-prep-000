4cd2a6840c5b7df163ec1f58e0f63980d81aef1a
lltchen <lltchen1@gmail.com>
2018-11-14 21:56:25 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,11 +1,1 @@
 var index = require("./index.js")
-
-if (index.name === "Susan") {
-    console.log("The name is correct")
-}
-
-
-if (index.height === "74") {
-    console.log("The height is correct")
-
-}
